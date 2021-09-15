8bd43a8ee23604f384c451bb426419c9a33f0b6e
Gabriel Pérez <gabriel@garox.org>
2021-09-15 12:45:10 -0300

    fix styles

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 
 import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>("#app");
+const app = document.querySelector<HTMLDivElement>(".reveal");
 
 if (!app) throw Error("No app div");
 
