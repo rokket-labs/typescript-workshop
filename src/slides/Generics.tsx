0d21af89483651225eb17720b2117fdd5f15d11f
Gabriel P??rez <gabriel@garox.org>
2021-09-22 19:38:54 -0300

    add more examples

--- src/slides/Generics.tsx
+++ src/slides/Generics.tsx
@@ -3,7 +3,10 @@ import React from "react";
 import TPose from "../assets/t_pose.gif";
 
 const codeAsync = /* ts */ `const fetchData = async (id: string): Promise<ApiResponse> => {
-  const response = await axios.get('/someEndpoint')
+  const response = await axios.get<ApiResponse>(
+    '/someEndpoint',
+    { id }
+    )
   return response.data
 }
 // (id: string) => Promise<ApiResponse>
@@ -13,7 +16,32 @@ const codeWithT = /* ts */ `type AxiosResponse<T> = {
   error: Error,
   data: T
 }
-// hola
+`;
+
+const codeWithoutT = /* ts */ `type AxiosResponse<ApiResponse> = {
+  error: Error,
+  data: ApiResponse
+}
+`;
+
+const codeReact = /* ts */ `type MyTypedComponentProps = {
+  name: string
+}
+
+const MyTypedComponent: FC<MyTypedComponentProps> = ({ name }) => {
+  return <p>{name}</p>
+}
+`;
+
+const codeUsingComponent = /* ts */ `// Error
+const WrongType: FC = () => {
+  return <MyTypedComponent name={5} />
+}
+
+// No error
+const CorrectType: FC = () => {
+  return <MyTypedComponent name={"5"} />
+}
 `;
 
 export const Generics = () => {
@@ -24,8 +52,34 @@ export const Generics = () => {
         <img src={TPose} />
       </section>
       <section>
-        <h2>T</h2>
-        <img src={TPose} />
+        <h2>"T" type</h2>
+        <pre>
+          <code>{codeWithT}</code>
+        </pre>
+      </section>
+      <section>
+        <pre>
+          <code>{codeWithoutT}</code>
+        </pre>
+      </section>
+      <section>
+        <pre>
+          <code>{codeAsync}</code>
+        </pre>
+      </section>
+      <section>
+        <pre>
+          <code className="language-tsx" data-line-numbers="1-3|5-7">
+            {codeReact}
+          </code>
+        </pre>
+      </section>
+      <section>
+        <pre>
+          <code className="language-tsx" data-line-numbers="1-4|6-9">
+            {codeUsingComponent}
+          </code>
+        </pre>
       </section>
     </section>
   );
